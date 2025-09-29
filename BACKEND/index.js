0f1392b8cb7630568bbab3fb1e84fadc132e3f29
prabdeep2005 <prabdeepsingh062@gmail.com>
2025-09-29 13:20:15 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import User from "./models/User.js";
+import User from "./models/user.js";
 
 dotenv.config();
 
